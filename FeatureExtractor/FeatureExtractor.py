08bc993ac5a172f7839c86109813f9e8e21f6513
Dylan <dylanlcornelius@gmail.com>
2018-10-22 19:04:05 -0400

    no message

--- FeatureExtractor/FeatureExtractor.py
+++ FeatureExtractor/FeatureExtractor.py
@@ -4,12 +4,16 @@
 #Term Project - r/randomactsofpizza
 
 import os
+from sklearn.feature_extraction.text import CountVectorizer
 
 path = os.path.dirname(os.path.realpath(__file__))
 
 #convert json to csv?
 
-lines = [line.rstrip('\n') for line in open(path + '\\train.json')]
+lines = [line.rstrip('\n') for line in open(path + '\\train.csv')]
 
 for line in lines:
     print(line)
+    #number of up-doots/down-doots
+    #text was edited
+    #text
