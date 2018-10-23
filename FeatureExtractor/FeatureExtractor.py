b47d579d9f009eea3b7ea3d83a899ae929b33e5e
Dylan <dylanlcornelius@gmail.com>
2018-10-22 20:07:52 -0400

    Reading from csv file

--- FeatureExtractor/FeatureExtractor.py
+++ FeatureExtractor/FeatureExtractor.py
@@ -4,16 +4,23 @@
 #Term Project - r/randomactsofpizza
 
 import os
+import csv
 from sklearn.feature_extraction.text import CountVectorizer
 
 path = os.path.dirname(os.path.realpath(__file__))
+dataFile = path + '\\train.csv'
 
-#convert json to csv?
+featFile = open(path + '\\PizzaFeatures.txt', 'w')
 
-lines = [line.rstrip('\n') for line in open(path + '\\train.csv')]
 
-for line in lines:
-    print(line)
+with open(dataFile, 'r', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader, None)
+    for row in reader:
+        featFile.write(repr((float(row[1])/ 0.000001 if float(row[2]) == 0 else float(row[2]))))
+    #print(line)
     #number of up-doots/down-doots
     #text was edited
     #text
+    #account age at request
+    #days since first post
